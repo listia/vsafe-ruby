bdd0dd40c952b5d6de01cbf66f6d8a5549057d93
Ngan Pham <nganpham@gmail.com>
2016-12-08 23:26:36 -0800

    Add some breathing room

--- spec/vsafe/config_spec.rb
+++ spec/vsafe/config_spec.rb
@@ -3,6 +3,7 @@ require "vsafe/config"
 
 RSpec.describe VSafe::Config do
   let (:config) { VSafe::Config.new }
+  
   describe ".new" do
     it "sets default configs" do
       expect(config.sandbox).to eq(true)
@@ -10,26 +11,31 @@ RSpec.describe VSafe::Config do
       expect(config.url).not_to be_empty
     end
   end
-  describe '#url' do
-    context 'sandbox is true' do
-      it 'uses default sandbox_url' do
+  
+  describe "#url" do
+    context "sandbox is true" do
+      it "uses default sandbox_url" do
         expect(config.url).to eq(VSafe::Config::DEFAULT_SANDBOX_URL)
       end
-      it 'uses custom sandbox_url when sandbox' do
-        config.sandbox_url = 'https://www.google.com'
-        expect(config.url).to eq('https://www.google.com')
+      
+      it "uses custom sandbox_url when sandbox" do
+        config.sandbox_url = "https://www.google.com"
+        expect(config.url).to eq("https://www.google.com")
       end
     end
-    context 'sandbox is not true' do
+    
+    context "sandbox is not true" do
       before do
         config.sandbox = false
       end
-      it 'uses default production_url when not sandbox' do
+      
+      it "uses default production_url when not sandbox" do
         expect(config.url).to eq(VSafe::Config::DEFAULT_PRODUCTION_URL)
       end
-      it 'uses custom production_url when not sandbox' do
-        config.production_url = 'https://www.google.com'
-        expect(config.url).to eq('https://www.google.com')
+      
+      it "uses custom production_url when not sandbox" do
+        config.production_url = "https://www.google.com"
+        expect(config.url).to eq("https://www.google.com")
       end
     end
   end
